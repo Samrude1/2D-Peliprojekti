bd902e673196ea9dc0f963a7ddef80713f760132
Samrude1 <126822763+Samrude1@users.noreply.github.com>
2023-03-13 09:44:34 +0200

    Basic Movements
    Nyt toimii kävely, juoksu, hyppy, kyykky + nopeudet

--- 2D Peliprojekti/Assets/Scripts/CharacterMovement.cs
+++ 2D Peliprojekti/Assets/Scripts/CharacterMovement.cs
@@ -6,16 +6,28 @@ public class CharacterMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] Transform groundCheckCollider;
+    [SerializeField] Transform overheadCheckCollider;
     [SerializeField] LayerMask groundLayer;
-    const float groundCheckRadius = 0.2f;
-    float horizontalMovement;
+    [SerializeField] BoxCollider2D standingCollider;
+    [SerializeField] CircleCollider2D crouchingCollider;
     [SerializeField] float speed = 1;
     [SerializeField] float jumpPower = 250;
+
+    const float groundCheckRadius = 0.2f;
+   //const float overheadCheckRadius = 0.2f; // Vaihdoin tän Raycastiin
+    float horizontalMovement;
+    public float runValue = 1f;
+    public float crouchValue = 0.5f;
+    public float headCheckLength; //Raycast
+
     private bool isFacingRight = true;
     public bool isGrounded = false;
     public bool isRunning = false;
-    public float runValue = 1f;
+    public bool isCrouched = false;
+    
     Animator animator;
+  
+    
 
     // Start is called before the first frame update
     void Awake()
@@ -28,14 +40,16 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
+        GroundCheck();
+        Crouch();
         Flip();
         Jump();
         Run();
+             
     }
 
     private void FixedUpdate()
     {
-        GroundCheck();
         Move();
         
     }
@@ -45,7 +59,7 @@ public class CharacterMovement : MonoBehaviour
         isGrounded = false;
 
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
-        if (colliders.Length > 0 )
+        if (colliders.Length > 0)
         {
             isGrounded = true;
         }
@@ -53,40 +67,35 @@ public class CharacterMovement : MonoBehaviour
 
     void Move()
     {
-        rb.velocity = new Vector2(horizontalMovement * speed * 100 * runValue * Time.fixedDeltaTime, rb.velocity.y);
-        
+        rb.velocity = new Vector2(horizontalMovement * speed * 100 * runValue * crouchValue  * Time.fixedDeltaTime, rb.velocity.y);
     }
 
     void Flip()
     {
-       
-
-        if(isFacingRight && horizontalMovement < 0f)
+        if (isFacingRight && horizontalMovement < 0f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             isFacingRight = false;
         }
-        else if(!isFacingRight && horizontalMovement > 0f)
+        else if (!isFacingRight && horizontalMovement > 0f)
         {
             transform.localScale = new Vector3(1, 1, 1);
             isFacingRight = true;
         }
-       
     }
 
     void Jump()
     {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            isGrounded= false;
+            isGrounded = false;
             rb.AddForce(new Vector2(0f, jumpPower));
         }
-        
     }
 
     void Run()
     {
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             isRunning = true;
         }
@@ -101,17 +110,53 @@ public class CharacterMovement : MonoBehaviour
         }
         else
         {
-            runValue= 1f;
+            runValue = 1f;
         }
         Debug.Log(rb.velocity.x);
         // 0 idle, 4 walk, 8 run
 
         animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+    }
+
+    void Crouch()
+    {
+        float yInput = Input.GetAxisRaw("Vertical");
+        bool isHeadHitting = HeadDetect();
+
+        if ((yInput < 0 || isHeadHitting) && isGrounded)
+        {
+            isCrouched = true;
+            animator.SetBool("Crouch", true);
+            standingCollider.enabled = false;
+            crouchingCollider.enabled = true;
+            crouchValue = 0.3f;
+        }
+        else //(!isHeadHitting)
+        {
+            isCrouched = false;
+            animator.SetBool("Crouch", false);
+            standingCollider.enabled = true;
+            crouchingCollider.enabled = false;
+            crouchValue = 1f;    
+        }
+
+        bool HeadDetect()
+        {
+            bool hit = Physics2D.Raycast(overheadCheckCollider.position, Vector2.up, headCheckLength, groundLayer);
+            return hit;
+        }
 
     }
 
+    private void OnDrawGizmos()
+    {
+        if (overheadCheckCollider == null) return;
 
+        Vector2 from = overheadCheckCollider.position;
+        Vector2 to = new Vector2(overheadCheckCollider.position.x, overheadCheckCollider.position.y + headCheckLength);
 
+        Gizmos.DrawLine(from, to);
+    }
 
 
 }
